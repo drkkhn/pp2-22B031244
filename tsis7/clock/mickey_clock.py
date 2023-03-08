62f21f3c78f5bfa15ec720641451cc2c9132dae5
Beket <dattebayokz@gmail.com>
2023-03-08 18:47:05 +0600

    finished mickey mouse clock

--- tsis7/clock/mickey_clock.py
+++ tsis7/clock/mickey_clock.py
@@ -1,30 +1,9 @@
-import pygame
+import pygame, sys
 from datetime import datetime
 import math
 
 
-def blit_rotate_center(surf, image, pos, orig_pos, angle):
-    image_rect = image.get_rect(center = (pos[0] - orig_pos[0], pos[1] - orig_pos[1]))
-    offset_center_to_pivot = pygame.math.Vector2(pos) - image_rect.center
 
-    rotated_offset = offset_center_to_pivot.rotate(angle)
-
-    rotated_image_center = (pos[0] - rotated_offset.x, pos[1] - rotated_offset.y)
-
-    rotated_image = pygame.transform.rotate(image, -angle)
-    rotated_image_rect = rotated_image.get_rect(center = rotated_image_center)
-
-    surf.blit(rotated_image, rotated_image_rect)
-    pygame.draw.rect(surf, (255, 0, 0), rotated_image_rect, 2)
-
-
-
-def blitRotate(surf, image, pos, angle):
-    rotated_image = pygame.transform.rotate(image, angle)
-    rotated_image_rect = rotated_image.get_rect(center = image.get_rect(topleft = pos).center)
-
-    surf.blit(rotated_image, rotated_image_rect.topleft)
-    pygame.draw.rect(surf, (255, 0, 0), rotated_image_rect, 2)
 
 
 def main():
@@ -36,37 +15,47 @@ def main():
     
     # get the current time
     curr_time = datetime.now()
+    curr_sec = curr_time.second
+    curr_min = curr_time.minute
 
-    # dict for minutes and seconds of a clock
-    clock60 = dict(zip(range(60), range(0, 360, 6)))
 
     # loading the images 
     clock_image = pygame.transform.scale(pygame.image.load('./images/mickeyclock.jpeg'), (800, 600))
-    seconds_hand_image = pygame.transform.scale(pygame.image.load('./images/seconds.png'), (150, 80))
-    minutes_hand_image = pygame.transform.scale(pygame.image.load('./images/minutes.png'), (130, 70))
+    sechand_image = pygame.image.load('./images/sechand.png')
+    sechand_image = pygame.transform.scale(sechand_image, (250, 75))
+    sechand_rect = sechand_image.get_rect()
+    sechand_rect.center = (400, 300)
+    minhand_image = pygame.image.load('./images/minhand.png')
+    minhand_image = pygame.transform.scale(minhand_image, (200, 50))
+    minhand_rect = minhand_image.get_rect()
+    minhand_rect.center = (400, 300)
 
-    angle = 0
-    seconds_pos = (clock_image.get_width() / 2, clock_image.get_height() / 2)
-    minutes_pos = (screen.get_width() / 2, screen.get_height() / 2)
-    w, h = seconds_hand_image.get_size()
-    isRunning = True
 
-    while isRunning:
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                isRunning = False
-
+                pygame.quit()
+                sys.quit()
         screen.fill(0)
-        pos = seconds_pos
-
         screen.blit(clock_image, (0, 0))
-        blit_rotate_center(screen, seconds_hand_image, seconds_pos, (w/2, h/2), angle) 
-        angle += 1
-                
+    
+        rot_minhand = pygame.transform.rotate(minhand_image, -1 * ((6 * curr_min) % 360))
+        rot_minhand_rect = rot_minhand.get_rect()
+        rot_minhand_rect.center = minhand_rect.center
+        screen.blit(rot_minhand, rot_minhand_rect)
+    
+        rot_sechand = pygame.transform.rotate(sechand_image, -1 * ((6 * curr_sec + 180) % 360))
+        rot_sechand_rect =rot_sechand.get_rect()
+        rot_sechand_rect.center = sechand_rect.center
+        screen.blit(rot_sechand, rot_sechand_rect)
 
-        pygame.display.flip()
-
-        clock.tick(24)
+        curr_time = datetime.now()
+        curr_sec = curr_time.second
+        curr_min = curr_time.minute
+        
+        pygame.display.update()
+        clock.tick(60)
 
 
 if __name__ == "__main__":
