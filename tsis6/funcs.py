9e53b6586a614c8c260a39a00aab93c729085cf5
Beket <dattebayokz@gmail.com>
2023-03-08 15:30:42 +0600

    some modifications to tsis6

--- tsis6/funcs.py
+++ tsis6/funcs.py
@@ -3,19 +3,39 @@ import re
 import time
 import math
 # Write a Python program with builtin function to multiply all the numbers in a list
-def multNums():
+def mult_nums():
     num_list = map(int, input().split())
     res = ft.reduce(lambda a, b: a*b, num_list)
     print(res)
 
+def mult_nums1():
+    num_list = map(int, input().split())
+    res = 1
+    for num in num_list:
+        res *= num
+    print(res)
+    
+
 # Write a Python program with builtin function that accepts a string and calculate the number of upper case letters and lower case letters
-def countLetters():
+def count_letters():
     inp_str = input()
     lwr_ptrn = re.compile("[a-z]")
     upr_ptrn = re.compile("[A-Z]")
     lwrs = lwr_ptrn.findall(inp_str)
     uprs = upr_ptrn.findall(inp_str)
     print(f"num of lower chars = {len(lwrs)}, num of upper chars = {len(uprs)}")
+
+def count_letters1():
+    inp_str = input()
+    count_lower = 0
+    count_upper = 0
+    for i in range(len(inp_str)):
+        if inp_str[i].islower():
+            count_lower += 1
+        elif inp_str[i].isupper():
+            count_upper += 1
+    print(f"num of lower chars = {count_lower}, num of upper chars = {count_upper}")
+
 
 # Write a Python program with builtin function that checks whether a passed string is palindrome or not.
 def isPalindrome(s):
@@ -36,11 +56,10 @@ def isAllTrue(inp_tuple):
     return all(inp_tuple)
 
 def main():
-    true_t = (2, "wordle", True)
-    false_t = (0, "wow", True)
-    print(isAllTrue(true_t))
-    print(isAllTrue(false_t))
-
+    print("Task 1")
+    mult_nums1()
+    print("Task 2")
+    count_letters1()
 
 if __name__ == "__main__":
     main() 
